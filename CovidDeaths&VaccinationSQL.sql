d3ec8b9612881df4dc046a153e427e6a83788872
Christodoulidou Nena <92935074+Chripart@users.noreply.github.com>
2024-01-24 13:18:27 +0200

    Update and rename SQLQuery1.sql to CovidDeaths&VaccinationSQL.sql

--- CovidDeaths&VaccinationSQL.sql
+++ CovidDeaths&VaccinationSQL.sql
@@ -166,15 +166,33 @@ select *
 from PercentPopulationVaccinated
 
 
+--Now, I will create some tables to use in my CovidDeaths Dashboard
 
+--Global Numbers
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
 
+--Total Death Count per Continent
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
 
+--Total Infection per Country
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
 
-
-
-
-
-
+--Percent of Population Infected per Month
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
 
 
 
